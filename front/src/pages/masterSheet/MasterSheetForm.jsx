1c6bd2268c5b1799a3d14ae429daf108c7166f71
Koushik Sarkar <koushiksarkar741777@gmail.com>
2026-04-28 15:14:57 +0530

    copy link some perfect

--- front/src/pages/masterSheet/MasterSheetForm.jsx
+++ front/src/pages/masterSheet/MasterSheetForm.jsx
@@ -95,20 +95,23 @@ const MasterSheetForm = ({ onSuccess, editRecord, onCancelEdit, isPublic = false
         <Paper elevation={0} sx={{ p: 4, borderRadius: 2, border: '1px solid', borderColor: 'divider' }}>
             <form onSubmit={formik.handleSubmit}>
                 <Grid container spacing={3}>
-                    {/* ACCREDITATION & CERTIFICATION */}
-                    <Grid item xs={12} md={6}>
-                        <TextField fullWidth select id="accreditationBody" name="accreditationBody" label="Accreditation Body" value={formik.values.accreditationBody} onChange={formik.handleChange} variant="outlined" size="small">
-                            {ACCREDITATION_BODIES.map((option) => (<MenuItem key={option} value={option}>{option}</MenuItem>))}
-                        </TextField>
-                    </Grid>
-                    {renderConditionalField("accreditationBody", "Accreditation Body")}
+                    {!isPublic && (
+                        <>
+                            <Grid item xs={12} md={6}>
+                                <TextField fullWidth select id="accreditationBody" name="accreditationBody" label="Accreditation Body" value={formik.values.accreditationBody} onChange={formik.handleChange} variant="outlined" size="small">
+                                    {ACCREDITATION_BODIES.map((option) => (<MenuItem key={option} value={option}>{option}</MenuItem>))}
+                                </TextField>
+                            </Grid>
+                            {renderConditionalField("accreditationBody", "Accreditation Body")}
 
-                    <Grid item xs={12} md={6}>
-                        <TextField fullWidth select id="certificationBody" name="certificationBody" label="Certification Body" value={formik.values.certificationBody} onChange={formik.handleChange} variant="outlined" size="small">
-                            {CERTIFICATION_BODIES.map((option) => (<MenuItem key={option} value={option}>{option}</MenuItem>))}
-                        </TextField>
-                    </Grid>
-                    {renderConditionalField("certificationBody", "Certification Body")}
+                            <Grid item xs={12} md={6}>
+                                <TextField fullWidth select id="certificationBody" name="certificationBody" label="Certification Body" value={formik.values.certificationBody} onChange={formik.handleChange} variant="outlined" size="small">
+                                    {CERTIFICATION_BODIES.map((option) => (<MenuItem key={option} value={option}>{option}</MenuItem>))}
+                                </TextField>
+                            </Grid>
+                            {renderConditionalField("certificationBody", "Certification Body")}
+                        </>
+                    )}
 
                     {/* SEARCHABLE DROPDOWN MENU */}
                     <Grid item xs={12} md={6}>
@@ -122,47 +125,59 @@ const MasterSheetForm = ({ onSuccess, editRecord, onCancelEdit, isPublic = false
                     </Grid>
                     {renderConditionalField("standard", "Standard")}
 
-                    {/* DATES SECTION */}
-                    <Grid item xs={12}><Divider sx={{ my: 1 }}>Dates</Divider></Grid>
-                    <Grid item xs={12} md={3}><TextField fullWidth type="date" id="issueDate" name="issueDate" label="Issue Date" InputLabelProps={{ shrink: true }} value={formik.values.issueDate} onChange={formik.handleChange} size="small" /></Grid>
-                    <Grid item xs={12} md={3}><TextField fullWidth type="date" id="firstSurvDate" name="firstSurvDate" label="1st Surveillance Date" InputLabelProps={{ shrink: true }} value={formik.values.firstSurvDate} onChange={formik.handleChange} size="small" /></Grid>
-                    <Grid item xs={12} md={3}><TextField fullWidth type="date" id="secondSurvDate" name="secondSurvDate" label="2nd Surveillance Date" InputLabelProps={{ shrink: true }} value={formik.values.secondSurvDate} onChange={formik.handleChange} size="small" /></Grid>
-                    <Grid item xs={12} md={3}><TextField fullWidth type="date" id="expireDate" name="expireDate" label="Expire Date" InputLabelProps={{ shrink: true }} value={formik.values.expireDate} onChange={formik.handleChange} size="small" /></Grid>
+                    {!isPublic && (
+                        <>
+                            <Grid item xs={12}><Divider sx={{ my: 1 }}>Dates</Divider></Grid>
+                            <Grid item xs={12} md={3}><TextField fullWidth type="date" id="issueDate" name="issueDate" label="Issue Date" InputLabelProps={{ shrink: true }} value={formik.values.issueDate} onChange={formik.handleChange} size="small" /></Grid>
+                            <Grid item xs={12} md={3}><TextField fullWidth type="date" id="firstSurvDate" name="firstSurvDate" label="1st Surveillance Date" InputLabelProps={{ shrink: true }} value={formik.values.firstSurvDate} onChange={formik.handleChange} size="small" /></Grid>
+                            <Grid item xs={12} md={3}><TextField fullWidth type="date" id="secondSurvDate" name="secondSurvDate" label="2nd Surveillance Date" InputLabelProps={{ shrink: true }} value={formik.values.secondSurvDate} onChange={formik.handleChange} size="small" /></Grid>
+                            <Grid item xs={12} md={3}><TextField fullWidth type="date" id="expireDate" name="expireDate" label="Expire Date" InputLabelProps={{ shrink: true }} value={formik.values.expireDate} onChange={formik.handleChange} size="small" /></Grid>
+                        </>
+                    )}
 
                     {/* COMPANY SECTION */}
                     <Grid item xs={12}><Divider sx={{ my: 1 }}>Company Details</Divider></Grid>
                     <Grid item xs={12} md={6}><TextField fullWidth id="companyName" name="companyName" label="Company Name" value={formik.values.companyName} onChange={formik.handleChange} error={formik.touched.companyName && Boolean(formik.errors.companyName)} helperText={formik.touched.companyName && formik.errors.companyName} size="small" /></Grid>
-                    <Grid item xs={12} md={6}><TextField fullWidth id="certificateNo" name="certificateNo" label="Certificate No." value={formik.values.certificateNo} onChange={formik.handleChange} size="small" /></Grid>
+                    {!isPublic && (
+                        <Grid item xs={12} md={6}><TextField fullWidth id="certificateNo" name="certificateNo" label="Certificate No." value={formik.values.certificateNo} onChange={formik.handleChange} size="small" /></Grid>
+                    )}
                     <Grid item xs={12} md={6}><TextField fullWidth multiline rows={2} id="address" name="address" label="Address" value={formik.values.address} onChange={formik.handleChange} size="small" /></Grid>
                     <Grid item xs={12} md={6}><TextField fullWidth multiline rows={2} id="scope" name="scope" label="Scope" value={formik.values.scope} onChange={formik.handleChange} size="small" /></Grid>
 
                     {/* CONTACT SECTION */}
                     <Grid item xs={12}><Divider sx={{ my: 1 }}>Contact Information</Divider></Grid>
-                    <Grid item xs={12} md={4}><TextField fullWidth id="contactPerson" name="contactPerson" label="Contact Person" value={formik.values.contactPerson} onChange={formik.handleChange} size="small" /></Grid>
                     <Grid item xs={12} md={4}><TextField fullWidth id="contactNumber" name="contactNumber" label="Contact Number" value={formik.values.contactNumber} onChange={formik.handleChange} size="small" /></Grid>
                     <Grid item xs={12} md={4}><TextField fullWidth id="emailId" name="emailId" label="Email ID" value={formik.values.emailId} onChange={formik.handleChange} error={formik.touched.emailId && Boolean(formik.errors.emailId)} helperText={formik.touched.emailId && formik.errors.emailId} size="small" /></Grid>
-                    <Grid item xs={12} md={4}><TextField fullWidth id="marketingExecutive" name="marketingExecutive" label="Marketing Executive" value={formik.values.marketingExecutive} onChange={formik.handleChange} size="small" /></Grid>
-                    <Grid item xs={12} md={4}><TextField fullWidth type="date" id="courierDate" name="courierDate" label="Courier Date" InputLabelProps={{ shrink: true }} value={formik.values.courierDate} onChange={formik.handleChange} size="small" /></Grid>
+                    {!isPublic && (
+                        <>
+                            <Grid item xs={12} md={4}><TextField fullWidth id="contactPerson" name="contactPerson" label="Contact Person" value={formik.values.contactPerson} onChange={formik.handleChange} size="small" /></Grid>
+                            <Grid item xs={12} md={4}><TextField fullWidth id="marketingExecutive" name="marketingExecutive" label="Marketing Executive" value={formik.values.marketingExecutive} onChange={formik.handleChange} size="small" /></Grid>
+                            <Grid item xs={12} md={4}><TextField fullWidth type="date" id="courierDate" name="courierDate" label="Courier Date" InputLabelProps={{ shrink: true }} value={formik.values.courierDate} onChange={formik.handleChange} size="small" /></Grid>
+                        </>
+                    )}
 
-                    {/* FINANCIAL SECTION */}
-                    <Grid item xs={12}><Divider sx={{ my: 1 }}>Financial & Client Details</Divider></Grid>
-                    <Grid item xs={12} md={4}>
-                        <TextField fullWidth select id="clientConsultant" name="clientConsultant" label="Client / Consultant" value={formik.values.clientConsultant} onChange={formik.handleChange} size="small">
-                            {CLIENT_CONSULTANT_OPTIONS.map((option) => (<MenuItem key={option} value={option}>{option}</MenuItem>))}
-                        </TextField>
-                    </Grid>
-                    {renderConditionalField("clientConsultant", "Type", "Other")}
+                    {!isPublic && (
+                        <>
+                            <Grid item xs={12}><Divider sx={{ my: 1 }}>Financial & Client Details</Divider></Grid>
+                            <Grid item xs={12} md={4}>
+                                <TextField fullWidth select id="clientConsultant" name="clientConsultant" label="Client / Consultant" value={formik.values.clientConsultant} onChange={formik.handleChange} size="small">
+                                    {CLIENT_CONSULTANT_OPTIONS.map((option) => (<MenuItem key={option} value={option}>{option}</MenuItem>))}
+                                </TextField>
+                            </Grid>
+                            {renderConditionalField("clientConsultant", "Type", "Other")}
 
-                    <Grid item xs={12} md={4}><TextField fullWidth id="amount" name="amount" label="Amount" type="number" value={formik.values.amount} onChange={formik.handleChange} error={formik.touched.amount && Boolean(formik.errors.amount)} helperText={formik.touched.amount && formik.errors.amount} size="small" /></Grid>
-                    
-                    <Grid item xs={12} md={4}>
-                        <TextField fullWidth select id="accountName" name="accountName" label="Account Name" value={formik.values.accountName} onChange={formik.handleChange} size="small">
-                            {ACCOUNT_NAMES.map((option) => (<MenuItem key={option} value={option}>{option}</MenuItem>))}
-                        </TextField>
-                    </Grid>
-                    {renderConditionalField("accountName", "Account Name")}
+                            <Grid item xs={12} md={4}><TextField fullWidth id="amount" name="amount" label="Amount" type="number" value={formik.values.amount} onChange={formik.handleChange} error={formik.touched.amount && Boolean(formik.errors.amount)} helperText={formik.touched.amount && formik.errors.amount} size="small" /></Grid>
+                            
+                            <Grid item xs={12} md={4}>
+                                <TextField fullWidth select id="accountName" name="accountName" label="Account Name" value={formik.values.accountName} onChange={formik.handleChange} size="small">
+                                    {ACCOUNT_NAMES.map((option) => (<MenuItem key={option} value={option}>{option}</MenuItem>))}
+                                </TextField>
+                            </Grid>
+                            {renderConditionalField("accountName", "Account Name")}
 
-                    <Grid item xs={12} md={4}><TextField fullWidth type="date" id="dateReceived" name="dateReceived" label="Date Received" InputLabelProps={{ shrink: true }} value={formik.values.dateReceived} onChange={formik.handleChange} size="small" /></Grid>
+                            <Grid item xs={12} md={4}><TextField fullWidth type="date" id="dateReceived" name="dateReceived" label="Date Received" InputLabelProps={{ shrink: true }} value={formik.values.dateReceived} onChange={formik.handleChange} size="small" /></Grid>
+                        </>
+                    )}
 
                     {/* SUBMIT, RESET & COPY LINK BUTTONS */}
                     <Grid item xs={12} sx={{ mt: 3, display: 'flex', gap: 2 }}>
